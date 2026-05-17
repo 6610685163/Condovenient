1acf8e6f1330510c185b90ba79d1bcc0cbc67f26
Chatchai Seekram <6610685064@cn102>
2026-05-17 20:24:09 +0700

    add getAllStaff and status history endpoints

--- condovenient_backend/src/routes/staffRoutes.js
+++ condovenient_backend/src/routes/staffRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const staffController = require('../controllers/staffController');
 
+router.get('/', staffController.getAllStaff);
 router.get('/:userId/status', staffController.getStaffStatus);
 router.patch('/:userId/status', staffController.updateWorkingStatus);
+router.get('/:userId/status/history', staffController.getStatusHistory);
 
 module.exports = router;
